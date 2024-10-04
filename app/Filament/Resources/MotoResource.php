ecb07c604bffcaac98644029a9f8e707a8926ea1
codeMountainEs <codemountain.es@icloud.com>
2024-10-04 13:03:13 +0200

    refactor db

--- app/Filament/Resources/MotoResource.php
+++ app/Filament/Resources/MotoResource.php
@@ -12,9 +12,10 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Illuminate\Support\Collection;
-
+use Livewire\Component;
 
 
 class MotoResource extends Resource
@@ -31,40 +32,41 @@ class MotoResource extends Resource
                 Forms\Components\Group::make()
                 ->relationship('user')
                 ->schema([
-                    Forms\Components\TextInput::make('name')
-                    ->label('Nombre')
-                ]),
 
-                Forms\Components\TextInput::make('matricula')
+
+                    Forms\Components\TextInput::make('matricula')
                     ->required()
                     ->unique()
+
                     ->maxLength(12),
-                Forms\Components\Select::make('marca_id')
+                    Forms\Components\TextInput::make('user_id')
+                    ->default(
+                        auth()->id()
+                    )
+
+                    ,
+
+                    Forms\Components\Select::make('marca_id')
                     ->relationship('marca', 'nombre')
                     ->searchable()
                     ->required()
                     ->live(onBlur: true)
                     ->createOptionForm([
                         Forms\Components\TextInput::make('nombre')
-                            ->required()]
+                            ->required()
+                        ]),
+                    Forms\Components\Select::make('modelo_moto_id')
+                        ->options(fn (Forms\Get $get): Collection => ModeloMoto::query()
+                            ->where('marca_id', $get('marca_id'))
+                            ->pluck('nombre', 'id'))
+                        ->createOptionForm([
+                            Forms\Components\TextInput::make('nombre')
+                                ->required()
+                        ]),
+                ])
 
-                    ),
-                Forms\Components\Select::make('modelo_moto_id')
-                    ->options(fn (Forms\Get $get): Collection => ModeloMoto::query()
-                        ->where('marca_id', $get('marca_id'))
-                        ->pluck('nombre', 'id'))
-                    ->createOptionForm([
-                        Forms\Components\TextInput::make('nombre')
-                            ->required()]
-                    ),
+               ]);
 
-
-
-
-
-
-
-            ]);
     }
 
     public static function table(Table $table): Table
